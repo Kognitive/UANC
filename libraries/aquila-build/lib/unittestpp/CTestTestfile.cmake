e9abab5618cf83490a2671051ce7d8eac88f22f8
markus <m.semmler.94@googlemail.com>
2016-11-17 18:55:44 +0100

    Adapted to Jens folder structure

--- libraries/aquila-build/lib/unittestpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/markus/Daten/University/BP/understanding-active-noise-cancelling/libraries/aquila-src/lib/unittestpp
-# Build directory: /home/markus/Daten/University/BP/understanding-active-noise-cancelling/libraries/aquila-build/lib/unittestpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestUnitTest++ "/home/markus/Daten/University/BP/understanding-active-noise-cancelling/libraries/aquila-build/lib/unittestpp/TestUnitTest++")
--- src/data_publisher/DataController.cc
+++
@@ -1,5 +0,0 @@
-//
-// Created by markus on 11/12/16.
-//
-
-#include "DataController.h"
