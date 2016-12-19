c336867775419fd18c37204923ed482e5a07a4d8
Daniel <danieljose.ceballosjung@stud.tu-darmstadt.de>
2016-12-19 12:59:23 +0100

    FFT

--- Code/UANC/algorithm/InverseFFTAlgorithm.h
+++ Code/UANC/algorithm/InverseFFTAlgorithm.h
@@ -8,6 +8,10 @@
 #include <Code/UANC/gui/model/FFTModel.h>
 #include <Code/UANC/gui/views/FFTView.h>
 #include "Algorithm.h"
+#include <complex>
+#include <Code/libs/aquila/source/SignalSource.h>
+#include <Code/libs/aquila/transform/FftFactory.h>
+
 
 namespace uanc {
 namespace algorithm {
@@ -32,8 +36,34 @@ class InverseFFTAlgorithm : public Algorithm {
    *
    * @return the processed vector itself.
    */
-   std::shared_ptr<Aquila::SignalSource> execute(std::shared_ptr<Aquila::SignalSource> in) {
-    return in;
+  std::shared_ptr<Aquila::SignalSource> execute(std::shared_ptr<Aquila::SignalSource> in) {
+    // map the input signal to a valid sample
+    auto SIZE = in->length();
+    auto sampleFreq = in->getSampleFrequency();
+
+    // choose a fft algorithm
+    auto fft = Aquila::FftFactory::getFft(SIZE);
+
+    // transform the signal into the fouier space
+    Aquila::SpectrumType spectrum = fft->fft(in->toArray());
+
+  /*
+    // invert the spectrum
+    std::transform(
+        spectrum.begin(),
+        spectrum.end(),
+        spectrum.begin(),
+        [](Aquila::ComplexType x) { return (x.operator*=(-1)); });
+  */
+
+    // back transformation of signal from fouier space
+    double x[SIZE];
+    fft->ifft(spectrum, x);
+
+    // define output signal
+    std::shared_ptr<Aquila::SignalSource> outputSignal(new Aquila::SignalSource(*x));
+
+    return outputSignal;
   }
 
   /** \brief Can be used to clone the algorithm.
@@ -41,16 +71,16 @@ class InverseFFTAlgorithm : public Algorithm {
    * This can be used to clone the algorithm
    * @return The cloned algorithm
    */
-  Algorithm* clone() {
+  Algorithm *clone() {
     new InverseFFTAlgorithm();
   }
  private:
-  uanc::gui::model::AlgorithmModel* constructModel() {
+  uanc::gui::model::AlgorithmModel *constructModel() {
     auto model = new uanc::gui::model::FFTModel();
     return model;
   }
 
-  uanc::gui::views::AlgorithmView* constructView() {
+  uanc::gui::views::AlgorithmView *constructView() {
     auto view = new uanc::gui::views::AlgorithmView();
     return view;
   }
