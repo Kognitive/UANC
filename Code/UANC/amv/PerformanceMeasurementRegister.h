96324a7c3e038ed0b893c5ccf2f2698cc6400299
Vladimir Roskin <vladimir@Vladi-Ultrabook>
2017-01-26 11:21:59 +0100

    <Performance Messen> Added missing file for Performance Measurement
    Change-Id: I737c8a97c150b1a3aff24db43593235f44e15c70

---
+++ Code/UANC/amv/PerformanceMeasurementRegister.h
@@ -0,0 +1,62 @@
+/*
+ * performanceMeasurementRegister.h
+ *
+ *  Created on: 23.01.2017
+ *      Author: vladimir
+ */
+
+#ifndef CODE_UANC_AMV_ANC_MODEL_PERFORMANCEMEASUREMENTREGISTER_H_
+#define CODE_UANC_AMV_ANC_MODEL_PERFORMANCEMEASUREMENTREGISTER_H_
+
+#include "../util/PerformanceMeasure.h"
+#include <vector>
+
+namespace uanc {
+namespace amv {
+namespace PMRegister {
+
+/** \brief This is an ANCModel.
+ *
+ * This is the class for registering new Performance Measurements for an Algorithm.
+ * It also contains the special default measurement which is executed for every class
+ * which inherits from ANCAlgorithm.
+ */
+class PerformanceMeasurementRegister {
+ public:
+
+  /**
+   *  This starts the execution of the overall Execution Measurement. This may be only executed once.
+   */
+  void startOverallExecutionMeasurement();
+
+  /**
+   *  Stops the overall execution measurement, if it is running
+   */
+  void stopOverallExecutionMeasurement();
+
+  /**
+   *  Registers a custom measurement.
+   *  @param measurement: The pointer to new measurement to register.
+   */
+  void registerCustomMeasurement(
+      std::shared_ptr<util::PerformanceMeasure<>> measurement);
+
+  const std::vector<std::shared_ptr<util::PerformanceMeasure<> > >& getCustomMeasurements() const {
+    return customMeasurements;
+  }
+
+  const std::shared_ptr<util::PerformanceMeasure<> >& getOverallMeasurement() const {
+    return overallMeasurement;
+  }
+
+ private:
+  std::vector<std::shared_ptr<util::PerformanceMeasure<>>> customMeasurements;
+  std::shared_ptr<util::PerformanceMeasure<>> overallMeasurement;
+
+};
+
+}
+}
+} /* namespace uanc */
+
+#endif /* CODE_UANC_AMV_ANC_MODEL_PERFORMANCEMEASUREMENTREGISTER_H_ */
